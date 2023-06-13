78959766719797061c6449decc9a81d9f987de48
FraFra2 <kekkino41@gmail.com>
2023-06-13 21:16:25 +0200

    MileStone 1

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//Html Elements
+const nameInput = document.querySelector("#nameInput");
+const kmInput = document.querySelector("#kmInput");
+const ageSel = document.querySelector("#ageSelect");
+
+const confirmBtn = document.querySelector("#generateBtn");
+
+let disc, price, discPrice;
+
+confirmBtn.addEventListener("click",
+    function(){
+        const nameValue = nameInput.value;
+        const kmValue = parseInt(kmInput.value);
+        const ageValue = ageSel.value;
+
+
+
+        if (ageValue == "Minorenne"){
+            disc = 0.8;
+        }else if(ageValue == "Over65"){
+            disc = 0.6;
+        }
+        else{
+            disc = 1;
+        }
+        price = kmValue * 0.21;
+        discPrice = price * disc;
+        console.log(price, discPrice);
+    }
+);
+
+
+
